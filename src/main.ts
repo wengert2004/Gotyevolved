85fec279e977b70392f4fd5fc1ced23e703fc9e4
wengert2004 <76923249+wengert2004@users.noreply.github.com>
2025-03-23 20:27:40 -0400

    adding images

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import Phaser from 'phaser';
+import packImg from '../assets/card_deck.webp';
+import card1Img from '../assets/aquatic_dragon.webp';
+import card2Img from '../assets/fire_fox.webp';
+import card3Img from '../assets/plant_dragon.webp';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#1d1d1d',
+  scene: {
+    preload,
+    create
+  }
+};
+
+function preload(this: Phaser.Scene) {
+  this.load.image('pack', packImg);
+  this.load.image('card1', card1Img);
+  this.load.image('card2', card2Img);
+  this.load.image('card3', card3Img);
+}
+
+function create(this: Phaser.Scene) {
+  const pack = this.add.image(400, 300, 'pack').setInteractive().setScale(0.5);
+
+  this.add.text(400, 100, 'Click to Open Pack', {
+    font: '24px Arial',
+    color: '#ffffff'
+  }).setOrigin(0.5);
+
+  pack.on('pointerdown', () => {
+    pack.destroy(); // remove pack image
+    this.add.image(250, 300, 'card1').setScale(0.4);
+    this.add.image(400, 300, 'card2').setScale(0.4);
+    this.add.image(550, 300, 'card3').setScale(0.4);
+  });
+}
+
+new Phaser.Game(config);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // Switch to 'production' for production builds
+    entry: './src/main.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+    },
+};
