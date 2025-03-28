196acd3ed9c3ed0445b55c16c02131714445c514
wengert2004 <76923249+wengert2004@users.noreply.github.com>
2025-03-28 19:42:32 -0400

    fixed directories

--- src/scenes/SlotMachineScene.ts
+++ src/scenes/SlotMachineScene.ts
@@ -1,5 +1,12 @@
 import Phaser from 'phaser'
 import { addHUD } from '../utils/SceneUI'
+import cherries from '../assets/slotmachinesceneimages/slot_cherries.png'
+import dragonFruit from '../assets/slotmachinesceneimages/slot_dragon_fruit.png'
+import grape from '../assets/slotmachinesceneimages/slot_grape.png'
+import lemon from '../assets/slotmachinesceneimages/slot_lemon.png'
+import pair from '../assets/slotmachinesceneimages/slot_pair.png'
+import pineapple from '../assets/slotmachinesceneimages/slot_pineapple.png'
+import cakeRoll from '../assets/slotmachinesceneimages/slot_strawberry_cake_roll.png'
 
 const GAME_WIDTH = 800
 const GAME_HEIGHT = 600
@@ -7,13 +14,13 @@ const REEL_WIDTH = 120
 const REEL_HEIGHT = 120
 
 const slotSymbols = [
-  'slot_cherries',
-  'slot_dragon_fruit',
-  'slot_grape',
-  'slot_lemon',
-  'slot_pair',
-  'slot_pineapple',
-  'slot_strawberry_cake_roll'
+  { key: 'slot_cherries', asset: cherries },
+  { key: 'slot_dragon_fruit', asset: dragonFruit },
+  { key: 'slot_grape', asset: grape },
+  { key: 'slot_lemon', asset: lemon },
+  { key: 'slot_pair', asset: pair },
+  { key: 'slot_pineapple', asset: pineapple },
+  { key: 'slot_strawberry_cake_roll', asset: cakeRoll }
 ]
 
 export class SlotMachineScene extends Phaser.Scene {
@@ -27,8 +34,9 @@ export class SlotMachineScene extends Phaser.Scene {
   }
 
   preload() {
+    // Load each image using its imported asset path
     slotSymbols.forEach(symbol => {
-      this.load.image(symbol, `assets/${symbol}.png`)
+      this.load.image(symbol.key, symbol.asset)
     })
   }
 
@@ -44,8 +52,8 @@ export class SlotMachineScene extends Phaser.Scene {
     for (let i = 0; i < 3; i++) {
       const column: Phaser.GameObjects.Image[] = []
       for (let j = 0; j < 3; j++) {
-        const key = Phaser.Utils.Array.GetRandom(slotSymbols)
-        const img = this.add.image(startX + i * 180, 150 + j * 130, key).setDisplaySize(REEL_WIDTH, REEL_HEIGHT)
+        const randomKey = Phaser.Utils.Array.GetRandom(slotSymbols).key
+        const img = this.add.image(startX + i * 180, 150 + j * 130, randomKey).setDisplaySize(REEL_WIDTH, REEL_HEIGHT)
         column.push(img)
       }
       this.reels.push(column)
@@ -74,19 +82,19 @@ export class SlotMachineScene extends Phaser.Scene {
     this.resultMessage.setText('')
 
     for (let i = 0; i < 3; i++) {
-      const symbol = Phaser.Utils.Array.GetRandom(slotSymbols)
+      const symbol = Phaser.Utils.Array.GetRandom(slotSymbols).key
       this.finalResults[i] = symbol
 
       for (let j = 0; j < 3; j++) {
         const delay = Phaser.Math.Between(50, 150)
         this.time.delayedCall(i * 150 + j * delay, () => {
-          const newKey = Phaser.Utils.Array.GetRandom(slotSymbols)
+          const newKey = Phaser.Utils.Array.GetRandom(slotSymbols).key
           this.reels[i][j].setTexture(newKey)
         })
       }
 
       this.time.delayedCall(600 + i * 150, () => {
-        this.reels[i][1].setTexture(symbol) // stop on final middle symbol
+        this.reels[i][1].setTexture(symbol)
       })
     }
 
@@ -106,9 +114,6 @@ export class SlotMachineScene extends Phaser.Scene {
     } else if (a === b || b === c || a === c) {
       message = '🥈 Small Win! You matched 2!'
       points = 25
-    } else if (a === c) {
-      message = '🎁 Bonus! Matching outer symbols!'
-      points = 40
     }
 
     this.resultMessage.setText(message)
