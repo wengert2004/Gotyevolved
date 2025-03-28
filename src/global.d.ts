6423bc5bc0ace1797898bbacd85deb81d7372eba
wengert2004 <76923249+wengert2004@users.noreply.github.com>
2025-03-28 19:32:20 -0400

    slot bug fix

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,8 @@ declare module '*.webp' {
     const content: string;
     export default content;
 }
+declare module '*.png' {
+    const value: string
+    export default value
+}
+
--- src/scenes/SlotMachineScene.ts
+++ src/scenes/SlotMachineScene.ts
@@ -1,123 +1,120 @@
-import Phaser from 'phaser';
-import { addHUD } from '../utils/SceneUI';
+import Phaser from 'phaser'
+import { addHUD } from '../utils/SceneUI'
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-const REEL_WIDTH = 120;
-const REEL_HEIGHT = 120;
+const GAME_WIDTH = 800
+const GAME_HEIGHT = 600
+const REEL_WIDTH = 120
+const REEL_HEIGHT = 120
 
-const imageKeys = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7'];
+const slotSymbols = [
+  'slot_cherries',
+  'slot_dragon_fruit',
+  'slot_grape',
+  'slot_lemon',
+  'slot_pair',
+  'slot_pineapple',
+  'slot_strawberry_cake_roll'
+]
 
 export class SlotMachineScene extends Phaser.Scene {
-    private resultMessage!: Phaser.GameObjects.Text;
-    private reels: Phaser.GameObjects.Image[][] = [];
-    private spinButton!: Phaser.GameObjects.Text;
-    private finalResults: string[] = [];
+  private resultMessage!: Phaser.GameObjects.Text
+  private reels: Phaser.GameObjects.Image[][] = []
+  private spinButton!: Phaser.GameObjects.Text
+  private finalResults: string[] = []
 
-    constructor() {
-        super('SlotMachineScene');
+  constructor() {
+    super('SlotMachineScene')
+  }
+
+  preload() {
+    slotSymbols.forEach(symbol => {
+      this.load.image(symbol, `assets/${symbol}.png`)
+    })
+  }
+
+  create() {
+    this.add.text(GAME_WIDTH / 2, 50, 'Slot Machine', {
+      fontSize: '32px',
+      color: '#ffffff'
+    }).setOrigin(0.5)
+
+    addHUD(this)
+
+    const startX = GAME_WIDTH / 2 - 180
+    for (let i = 0; i < 3; i++) {
+      const column: Phaser.GameObjects.Image[] = []
+      for (let j = 0; j < 3; j++) {
+        const key = Phaser.Utils.Array.GetRandom(slotSymbols)
+        const img = this.add.image(startX + i * 180, 150 + j * 130, key).setDisplaySize(REEL_WIDTH, REEL_HEIGHT)
+        column.push(img)
+      }
+      this.reels.push(column)
     }
 
-    preload() {
-        const urls = [
-            'https://imgur.com/d4azBtn.png',
-            'https://imgur.com/FcywbAs.png',
-            'https://imgur.com/A1ZyDLs.png',
-            'https://imgur.com/jsXAQqt.png',
-            'https://imgur.com/VjNDVcs.png',
-            'https://imgur.com/gzGgLz8.png',
-            'https://imgur.com/PnjGXuY.png'
-        ];
+    const buttonRect = this.add.rectangle(GAME_WIDTH / 2, 520, 160, 50, 0x4CAF50)
+      .setInteractive({ useHandCursor: true })
 
-        urls.forEach((url, i) => {
-            this.load.image(imageKeys[i], url);
-        });
+    this.spinButton = this.add.text(GAME_WIDTH / 2, 520, 'SPIN', {
+      fontSize: '24px',
+      color: '#ffffff'
+    }).setOrigin(0.5)
+
+    buttonRect.on('pointerdown', () => {
+      this.spin()
+    })
+
+    this.resultMessage = this.add.text(GAME_WIDTH / 2, 580, '', {
+      fontSize: '24px',
+      color: '#ffffff'
+    }).setOrigin(0.5)
+  }
+
+  private spin() {
+    this.finalResults = []
+    this.resultMessage.setText('')
+
+    for (let i = 0; i < 3; i++) {
+      const symbol = Phaser.Utils.Array.GetRandom(slotSymbols)
+      this.finalResults[i] = symbol
+
+      for (let j = 0; j < 3; j++) {
+        const delay = Phaser.Math.Between(50, 150)
+        this.time.delayedCall(i * 150 + j * delay, () => {
+          const newKey = Phaser.Utils.Array.GetRandom(slotSymbols)
+          this.reels[i][j].setTexture(newKey)
+        })
+      }
+
+      this.time.delayedCall(600 + i * 150, () => {
+        this.reels[i][1].setTexture(symbol) // stop on final middle symbol
+      })
     }
 
-    create() {
-        this.add.text(GAME_WIDTH / 2, 50, 'Slot Machine', {
-            fontSize: '32px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
+    this.time.delayedCall(1300, () => {
+      this.evaluate()
+    })
+  }
 
-        addHUD(this);
+  private evaluate() {
+    const [a, b, c] = this.finalResults
+    let points = 0
+    let message = '❌ No win'
 
-        // Setup reels
-        const startX = GAME_WIDTH / 2 - 180;
-        for (let i = 0; i < 3; i++) {
-            const column: Phaser.GameObjects.Image[] = [];
-            for (let j = 0; j < 3; j++) {
-                const key = Phaser.Utils.Array.GetRandom(imageKeys);
-                const img = this.add.image(startX + i * 180, 150 + j * 130, key).setDisplaySize(REEL_WIDTH, REEL_HEIGHT);
-                column.push(img);
-            }
-            this.reels.push(column);
-        }
-
-        const buttonRect = this.add.rectangle(GAME_WIDTH / 2, 520, 160, 50, 0x4CAF50)
-            .setInteractive({ useHandCursor: true });
-
-        this.spinButton = this.add.text(GAME_WIDTH / 2, 520, 'SPIN', {
-            fontSize: '24px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
-
-        buttonRect.on('pointerdown', () => {
-            this.spin();
-        });
-
-        this.resultMessage = this.add.text(GAME_WIDTH / 2, 580, '', {
-            fontSize: '24px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
+    if (a === b && b === c) {
+      message = '🎉 JACKPOT! You matched all 3!'
+      points = 100
+    } else if (a === b || b === c || a === c) {
+      message = '🥈 Small Win! You matched 2!'
+      points = 25
+    } else if (a === c) {
+      message = '🎁 Bonus! Matching outer symbols!'
+      points = 40
     }
 
-    private spin() {
-        this.finalResults = [];
-        this.resultMessage.setText('');
+    this.resultMessage.setText(message)
 
-        for (let i = 0; i < 3; i++) {
-            const symbol = Phaser.Utils.Array.GetRandom(imageKeys);
-            this.finalResults[i] = symbol;
-
-            for (let j = 0; j < 3; j++) {
-                const delay = Phaser.Math.Between(50, 150);
-                this.time.delayedCall(i * 150 + j * delay, () => {
-                    const newKey = Phaser.Utils.Array.GetRandom(imageKeys);
-                    this.reels[i][j].setTexture(newKey);
-                });
-            }
-
-            this.time.delayedCall(600 + i * 150, () => {
-                this.reels[i][1].setTexture(symbol); // final center symbol
-            });
-        }
-
-        this.time.delayedCall(1300, () => {
-            this.evaluate();
-        });
-    }
-
-    private evaluate() {
-        const [a, b, c] = this.finalResults;
-        let points = 0;
-        let message = '❌ No win';
-
-        if (a === b && b === c) {
-            message = '🎉 JACKPOT! You matched all 3!';
-            points = 100;
-        } else if (a === b || b === c || a === c) {
-            message = '🥈 Small Win! You matched 2!';
-            points = 25;
-        } else if (a === c) {
-            message = '🎁 Bonus! Matching outer symbols!';
-            points = 40;
-        }
-
-        this.resultMessage.setText(message);
-
-        const currentPoints = this.registry.get('totalPoints') || 0;
-        this.registry.set('totalPoints', currentPoints + points);
-        this.events.emit('updatePoints');
-    }
+    const currentPoints = this.registry.get('totalPoints') || 0
+    this.registry.set('totalPoints', currentPoints + points)
+    this.events.emit('updatePoints')
+  }
 }
